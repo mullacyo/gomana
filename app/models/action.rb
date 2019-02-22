d4db8caf5007839c24eaa33afd782fa304b7961b
Callum Tan <mullacyo@hotmail.com>
2019-02-22 15:27:26 +0800

    added bootstrap and css to it

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,10 +1,4 @@
 class Action < ApplicationRecord
-    validates :name, presence: true
-    # validates :type
-    validates :description, presence: true
-    validates :location, presence: true
-    validates :duration, presence: true
-
     has_many :actions_outings
     has_many :outings, through: :actions_outings
     # scope :location, -> (location) { where("location ILIKE ?", "%#{location}%") }
