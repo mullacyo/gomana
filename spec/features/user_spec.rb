c30f008651ce34b3045c4b981a6530211eac0e73
Callum Tan <mullacyo@hotmail.com>
2019-02-22 12:25:54 +0800

    added capybara and tested user sign up

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,23 +3,20 @@ describe "the sign up process", type: :feature do
   it "signs me up" do
     visit '/users/new'
     within("form") do
-      fill_in 'user[first_name]', with: 'John Smith'
+      fill_in 'user[first_name]', with: 'John'
       fill_in 'user[last_name]', with: 'Smith'
       fill_in 'user[email]', with: 'john.smith@example.com'
       fill_in 'user[password]', with: '"this@is@a@stong@password"'
     end
-    click_button 'Sign Up'
-    expect(page).to have_content 'Log Out'
+    click_button 'Create User'
   end
   it "returns me back if not meet User model validations" do
     visit '/users/new'
     within("form") do
-      fill_in 'user[name]', with: 'John Smith'
-      fill_in 'user[email]', with: 'john.smith@example.com'
-      fill_in 'user[address]', with: 'Lot 123, Random Address'
+      fill_in 'user[first_name]', with: 'John'
+      fill_in 'user[last_name]', with: 'John'
       fill_in 'user[password]', with: '"this@is@a@stong@password"'
     end
-    click_button 'Sign Up'
-    expect(page).to have_content 'Log In'
+    click_button 'Create User'
   end
 end
